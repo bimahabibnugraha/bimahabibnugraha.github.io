f39af46a7f569e43db1dc38c36b4a8a7d7b64c57
bimahabibnugraha <165061378+bimahabibnugraha@users.noreply.github.com>
2025-12-04 11:58:28 +0700

    Update email_hse.js

--- cover/templates/email_hse.js
+++ cover/templates/email_hse.js
@@ -6,7 +6,7 @@ const EMAIL_HSE = {
     body: `Yth. Bapak/Ibu HRD/Hiring Manager **{{COMPANY}}**,
 
 Dengan hormat,
-Perkenalkan, saya **{{NAMA}}**, lulusan Sarjana Teknik yang memiliki sertifikasi Ahli K3 Umum (Kemnaker RI). Saya tertarik untuk mengisi posisi {{POSITION}} di **{{COMPANY}}**.
+Perkenalkan, saya **{{NAMA}}**, lulusan Sarjana Teknik yang memiliki sertifikasi **Ahli K3 Umum (Kemnaker RI)**. Saya tertarik untuk mengisi posisi **{{POSITION}}** di **{{COMPANY}}**.
 Latar belakang teknik saya melatih saya berpikir analitis dalam mengidentifikasi risiko, sementara pengalaman saya memimpin proyek pengembangan geowisata melatih saya dalam manajemen lapangan dan komunikasi di lapangan—dua hal krusial dalam penerapan budaya keselamatan kerja (safety culture).
 
 Bersama ini saya lampirkan CV, sertifikat kompetensi dan dokumen pendukung untuk Bapak/Ibu tinjau. Saya siap ditempatkan di lokasi proyek dimanapun sesuai kebutuhan perusahaan.
